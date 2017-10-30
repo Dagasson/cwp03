2920e0efeb9da841678807258d229e7a31609a5c
Unknown <mushuk-artur@mail.ru>
2017-10-30 08:43:48 +0300

    Lab is working.

--- client-files.js
+++ client-files.js
@@ -4,10 +4,9 @@ const port = 8124;
 const path=require('path');
 
 const client = new net.Socket();
-let ind = 0;
 let adr=[];
 let separator = "\t\v\t\r";
-
+let stack=[];
 const dec = "DEC";
 const ack = "ACK";
 const fl = "FILES";
@@ -15,27 +14,33 @@ const unknown = "UNKNOWN";
 const dir = "Direct";
 const client_name = "Client";
 const server_name = "Server";
+const accept = "File received";
 
 client.setEncoding('utf8');
 
 client.connect(port, function() {
             client.write(fl);
             console.log('Connected to the server');
-     get_adress(adr);   
+     get_adress(adr);
 });
 client.on('data', function (data){
-	if(data===ack) 
+	if(data===ack)
 	{
 		console.log("connection established");
 		send_file(adr);
 	}
+	if (stack.length !== 0) {
+        sendtoserver();
+    }
+
 });
+
 
 function get_adress(adr, err){
 	if (err) console.log("Bad adress.")
 	for(var i=2; i<process.argv.length; i++){
 		adr[i-2]= process.argv[i];
-	}	
+	}
 	for (let i=0; i<adr.length;i++) console.log(adr[i]);
 }
 
@@ -45,23 +50,30 @@ function send_file(adr, err){
 	for(let ind=0; ind<adr.length; ind++)
 	{
 		console.log("Start sending.");
-		fs.readdirSync(adr[ind], function(err, files) {
+		fs.readdirSync(adr[ind]).forEach((files)=> {
 		if(err) console.log("Error reading directory.");
-		else
-		{
-			console.log("Working with directory:"+adr[ind]);
-			for(let i in files)
-				{
-					let iffiles=pat+'\\'+files[i];
-					if(fs.statSync(iffiles).isDirectory()) send_file(iffiles);
-					else
-					{
-						client.write(files[i]);
-						console.log("Sending files.");
-                    }
-				}
-			}
+		else {
+            console.log("Reading directories.");
+
+                let iffiles = adr[ind] + '\\' + files;
+                if (fs.statSync(iffiles).isDirectory()) send_file(iffiles);
+                else {
+                    stack.push(iffiles);
+                    console.log("Sending file.");
+                }
+
+        }
 	});
-    }}
+    }
+    console.log("stack length:"+stack.length);
+	}
 }
-	
+
+function sendtoserver()
+{
+console.log("send to server");
+	let forsend=stack.pop();
+	client.write(fs.readFileSync(forsend));
+	client.write(separator+path.basename(forsend));
+	client.write(separator+"FIN");
+}
--- server.js
+++ server.js
@@ -1,50 +1,40 @@
-const net = require('net');
-const fs = require("fs");
 const path = require('path');
+const net = require('net');
+const fs = require('fs');
 const port = 8124;
-let seed = 0;
 const saveDirectory = process.env.NODE_PATH;
 const maxConnection = process.env.CONST_MAX_CONNECTION;
 
+const files = "FILES";
+const ack = "ACK";
 const dec = "DEC";
-const ack= "ACK";
-const fl = "FILES";
-const unknown = "UNKNOWN";
-const dir = "Direct";
-const client_name = "Client";
-const server_name = "Server";
-let separator = "\t\v\t\r";
-let file=[];
+const accept = "File received";
+
+let seed = 0;
 let clientModes = [];
+let file = [];
+let separator = "\t\v\t\r";
 
-const server = net.createServer((client) =>
-{
-    console.log('Client connected');
+const server = net.createServer((client) => {
 
-    client.setEncoding('utf8');
 
-    client.on('data', (data) =>
-    {
-        if (client.id === undefined&&data.toString()===fl)
-        {
-            client.id = (Date.now() + seed++).toString();
-			client.write(ack);
-            console.log("id : " + client.id);
+    client.on('data', (data) => {
+        if (data.toString() === dec) {
+            console.log("UNDEFINED DISCONNECT FROM SERVER BY DEC COMMAND");
+            client.write(dec);
+            client.end();
         }
-		else 
-		{
-			client.write(dec);
-			client.end();
-		}
     });
     client.on('data', ClientHandler);
     client.on('data', ClientFilesDialogue);
+    client.on('end', () => console.log(`Client ${client.id} disconnected`));
 
-    client.on('end', () => console.log('Client disconnected'));
 
-	function ClientHandler(data, error) {
+
+    function ClientHandler(data, error) {
         if (!error) {
-            if (client.id === undefined && (data.toString() === fl)) {
+            console.log("client handler")
+            if (client.id === undefined && (data.toString() === files)) {
                 client.id = Date.now().toString() + seed++;
                 console.log(`Client ${client.id} connect`);
                 clientModes[client.id] = data.toString();
@@ -60,7 +50,7 @@ const server = net.createServer((client) =>
     }
     function ClientFilesDialogue(data, error) {
         if (!error) {
-            if (clientModes[client.id] === fl && data.toString() !== "FILES") {
+            if (clientModes[client.id] === files && data.toString() !== "FILES") {
                 file[client.id].push(data);
                 if (data.toString().endsWith(separator + "FIN")) {
                     CreateFile(saveDirectory + path.sep + client.id, client.id);
@@ -72,17 +62,15 @@ const server = net.createServer((client) =>
             console.error("ClientFilesDialogue error : " + error);
         }
     }
-
 });
 server.maxConnections = maxConnection;
 
-server.listen(port, () =>
-{
-    console.log(`Server listening on localhost:${port}`)	
-    });
-
+server.listen(port, () => {
+    console.log(`Server listening on localhost:${port}`);
+});
 
 function CreateFile(saveDir, id) {
+    console.log("createfile");
     let buffer = Buffer.concat(file[id]);
     let separatorIndex = buffer.indexOf(separator);
     let filename = buffer.slice(separatorIndex).toString().split(separator).filter(Boolean)[0];
